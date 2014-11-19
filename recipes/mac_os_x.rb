52425e37f941b5f6e7bae00142bc2380a8487c02
jtimberman <joshua@opscode.com>
2014-11-19 08:10:01 -0700

    use full octal for modes

--- recipes/mac_os_x.rb
+++ recipes/mac_os_x.rb
@@ -9,11 +9,11 @@ chef_gem 'plist'
 ohai('system_profile') { plugin 'system_profile' }.run_action(:reload)
 
 directory '/opt/homebrew-cask' do
-  mode 0775
+  mode 00775
 end
 
 directory '/opt/homebrew-cask/Caskroom' do
-  mode 0775
+  mode 00775
 end
 
 # Chef::Provider::User::Dscl uses this directory, but nothing actually creates it by default.
