16b6141eb81ccdb0cfad8c26d21a03c6b0642eca
jtimberman <joshua@chef.io>
2014-12-10 22:18:15 -0700

    ensure Library/Caches is sticky for cask

--- recipes/mac_os_x.rb
+++ recipes/mac_os_x.rb
@@ -16,6 +16,10 @@ directory '/opt/homebrew-cask/Caskroom' do
   mode 00775
 end
 
+directory '/Library/Caches' do
+  mode 01777
+end
+
 # Chef::Provider::User::Dscl uses this directory, but nothing actually creates it by default.
 # https://github.com/opscode/chef/issues/1634
 directory '/var/db/shadow/hash' do
