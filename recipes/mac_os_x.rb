aec2f71642aa88d28694eb7772919eb2058453b2
Jose Diaz-Gonzalez <josegonzalez@users.noreply.github.com>
2015-01-22 14:41:45 -0500

    Run homebrew::install_taps

--- recipes/mac_os_x.rb
+++ recipes/mac_os_x.rb
@@ -12,5 +12,6 @@ end
 
 include_recipe 'homebrew'
 include_recipe 'homebrew::cask'
+include_recipe 'homebrew::install_taps'
 include_recipe 'homebrew::install_formulas'
 include_recipe 'homebrew::install_casks'
