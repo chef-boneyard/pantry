b1391a7971d473fdd922549196f9072ddf9e1440
Tim Smith <tsmith84@gmail.com>
2015-09-09 20:41:37 -0700

    Add supermarket metadata

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,11 @@
 name              'pantry'
 maintainer        'Joshua Timberman'
-maintainer_email  'joshua@getchef.com'
+maintainer_email  'joshua@chef.io'
 license           'Apache 2.0'
 description       'Chef Pantry is a workstation automation cookbook'
 version           '0.2.1'
 depends           'homebrew', '~> 1.12'
 depends           'chocolatey', '~> 0.2'
+
+source_url 'https://github.com/chef-cookbooks/pantry' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/pantry/issues' if respond_to?(:issues_url)
