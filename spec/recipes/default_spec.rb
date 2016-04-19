973450a5cbe76d51da8dae6dc263e42370d10101
Oliver Ferrigni <oliver@chef.io>
2016-04-19 08:35:05 -0500

    Mock inclusion of windows recipe

--- spec/recipes/default_spec.rb
+++ spec/recipes/default_spec.rb
@@ -18,13 +18,18 @@ describe 'pantry::default' do
   end
 
   context 'Windows' do
+    before do
+      allow_any_instance_of(Chef::Recipe).to receive(:include_recipe).and_call_original
+      allow_any_instance_of(Chef::Recipe).to receive(:include_recipe).with('pantry::windows')
+    end
     let(:chef_run) do
       ChefSpec::ServerRunner.new(platform: 'windows', version: '2012')
         .converge(described_recipe)
     end
 
     it 'includes pantry::windows recipe' do
-      expect(chef_run).to include_recipe('pantry::windows')
+      expect_any_instance_of(Chef::Recipe).to receive(:include_recipe).with('pantry::windows')
+      chef_run
     end
   end
 
