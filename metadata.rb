aee88358000b8127bdfef628c77773d94e641905
Oliver Ferrigni <oliver@chef.io>
2016-04-19 20:27:12 -0500

    Bump Version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        'Joshua Timberman'
 maintainer_email  'joshua@chef.io'
 license           'Apache 2.0'
 description       'Chef Pantry is a workstation automation cookbook'
-version           '0.2.1'
+version           '0.2.2'
 depends           'homebrew', '~> 2.1'
 depends           'chocolatey', '~> 0.2'
 
