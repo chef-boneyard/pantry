932df8979c07795d2e3b12a2fae1644227adca54
Tim Smith <tsmith@chef.io>
2016-09-15 11:43:37 -0700

    Remove version constraints and depend on build-essential
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,9 @@ maintainer_email  'joshua@chef.io'
 license           'Apache 2.0'
 description       'Chef Pantry is a workstation automation cookbook'
 version           '0.2.2'
-depends           'homebrew', '~> 2.1'
-depends           'chocolatey', '~> 0.2'
+depends           'homebrew'
+depends           'build-essential'
+depends           'chocolatey'
 
 source_url 'https://github.com/chef-cookbooks/pantry' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-cookbooks/pantry/issues' if respond_to?(:issues_url)
